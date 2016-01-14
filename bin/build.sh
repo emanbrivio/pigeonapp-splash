fd210fb8d2f197b9d40a1b662d9e860e05585ab0
Luca Casartelli <luca@plasticpanda.com>
2016-01-14 16:51:12 +0100

    add send-code form

--- bin/build.sh
+++ bin/build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-API_URL=https://api.pigeonapp.im/form/create
-echo $API_URL
-JADE_OBJECT="{\"API_URL\":  \"$API_URL\"}"
+BASE_URL=https://api.pigeonapp.im
+CREATE_FORM_PATH=/form/create
+SEND_CODE_PATH=/p/
+JADE_OBJECT="{\"BASE_URL\":  \"$BASE_URL\", \"CREATE_FORM_PATH\":  \"$CREATE_FORM_PATH\", \"SEND_CODE_PATH\":  \"$SEND_CODE_PATH\" }"
 ./node_modules/.bin/http-server -c-1 -p $PORT -a 0.0.0.0 &
 ./node_modules/.bin/jade *.jade **/*.jade -O "$JADE_OBJECT" --watch &
 ./node_modules/.bin/stylus --use nib --use rupture -c -w ./static/css/
